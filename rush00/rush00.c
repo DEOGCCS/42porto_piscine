6fdfd31a91185d4017c5d217d9b4864667d77343
David Gomes <davidoliveiraccs@gmail.com>
2025-07-19 17:24:51 +0100

    Update rush00.c

--- rush00/rush00.c
+++ rush00/rush00.c
@@ -1,8 +1,39 @@
 #include <unistd.h>
 
-// tem de ter a função de ft_putchar
-void  ft_putchar(char c);
+// função de ft_putchar
+void    ft_putchar(char c);
+
+int     main(void)   
 {
+        char c;
+
+        c = 'c';
+
         write(1, &c, 1);
         return (0);
+}
+
+// Função do rush
+int        rush(char l)
 {
+        char line = l;
+        l = 'A';
+        while( l >= '1')
+        {
+        write(1, &l, 1);
+        l--        
+        }
+        while( l < l)
+        {
+        write(1, &l, 1);
+        l+        
+        }
+
+
+// função de main
+
+int        main()
+{
+        rush(5, 5);
+        returno (0);
+}
