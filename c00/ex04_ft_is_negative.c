246644f502e851fda1884a9f0b7cf2b0f47a7ae6
David Gomes <davidoliveiraccs@gmail.com>
2025-07-19 17:56:45 +0100

    Create ex04_ft_is_negative.c

---
+++ c00/ex04_ft_is_negative.c
@@ -0,0 +1,13 @@
+#include <unstd.h>
+
+void  ft_is_negative(int n);
+
+int  main()
+{
+        char n = 'N';
+
+        if  (write(1, &n, 1) == -1)
+        {
+        write(2, "P", 1);
+        }
+}
