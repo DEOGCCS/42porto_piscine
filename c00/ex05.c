1480a782f139b64efc58a07749a18584914354bf
David Gomes <davidoliveiraccs@gmail.com>
2025-07-22 11:09:28 +0100

    Update ex05.c

--- c00/ex05.c
+++ c00/ex05.c
@@ -2,13 +2,13 @@
 
 int ft_print_comb(void)
 {
-    char    numb[3] <= '789';
+    int    numb[3] <= 789;
 
-    numb[0] = numb[0] >= '0' && numb[0] <= '7';
-    numb[1] = numb[1] >= '1' && numb[0] <= '8';
-    numb[2] = numb[0] >= '2' && numb[0] <= '9':
+    numb[0] = numb[0] >= 0 && numb[0] <= 7;
+    numb[1] = numb[1] >= 0 && numb[0] <= 8;
+    numb[2] = numb[0] >= 2 && numb[0] <= 9;
 
-    if ( numb[0] <= '7' && numb[1] <= '8' && numb[2] <= '9')
+    if ( numb[0] <= 7 && numb[1] <= 8 && numb[2] <= 9)
     {
         write(1, &numb, 3);
         numb++
