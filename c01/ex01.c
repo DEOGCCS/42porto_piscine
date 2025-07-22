05adb49a45ca2adee8ce2090234020947fa5ba72
David Gomes <davidoliveiraccs@gmail.com>
2025-07-22 15:34:55 +0100

    Create ex01.c

---
+++ c01/ex01.c
@@ -0,0 +1,24 @@
+//# include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main()
+{
+	int 	n = 42;
+	int	*nbr1 = &n;
+        int	**nbr2 = &nbr1;
+        int	***nbr3 = &nbr2;
+        int	****nbr4 = &nbr3;
+        int	*****nbr5 = &nbr4;
+        int	******nbr6 = &nbr5;
+        int	*******nbr7 = &nbr6;
+        int	********nbr8 = &nbr7;
+        int	*********nbr = &nbr8;
+
+	ft_ultimate_ft(nbr);
+	printf("%d\n", n);
+	return (0);
+}*/
