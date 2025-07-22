2c920889a7139340a1293ea7a84744c10f8b2e33
David Gomes <davidoliveiraccs@gmail.com>
2025-07-22 15:52:11 +0100

    Create ex02.c

---
+++ c01/ex02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void    ft_swap(int *a, int *b)
+{       
+        int *a = 4;
+        int *b = 2;
+}       
+
+int     main()
+{       
+    int     c = 4;
+    int    *a = &c;
+    
+     int    d = 2;
+     int   *b = &d;
+    
+        ft_swap(a)
+        printf("%d\n", a);
+        return (0);
+}       
+ 
