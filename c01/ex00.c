300673137eacc64384afa5e526946c6c45883a03
David Gomes <davidoliveiraccs@gmail.com>
2025-07-22 15:02:20 +0100

    Update ex00.c

--- c01/ex00.c
+++ c01/ex00.c
@@ -2,13 +2,5 @@
 
 void    ft_ft(int *nbr)
 {       
-        int     a = 42;
-        nbr = &a;
-        write (1, &nbr, 2);
-}
-
-int     main(void)
-{       
-        ft_ft();
-        return (0);
+        *nbr = 42;
 }
