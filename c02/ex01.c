bf7cf880ed4a0d47f0269e8a15dc13604d581660
David Gomes <davidoliveiraccs@gmail.com>
2025-07-25 13:26:03 +0100

    Create ex01.c

---
+++ c02/ex01.c
@@ -0,0 +1,31 @@
+/*
+#include <stdio.h>
+*/
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (dest[i] == '\0')
+	{
+		i++;
+	}
+	return (dest);
+}
+/*
+int	main()
+{
+	char src[] = "hello";
+	char dest[10];
+
+	printf("Scroce: '%s'\n", src);
+	ft_strncpy(dest, src, 8);
+	printf("Destination: '%s'\n", dest);
+
+	 return 0;
+}*/
