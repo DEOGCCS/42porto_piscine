3320c7690522b0091fc5ceac0c14fb8b8ca13316
David Gomes <davidoliveiraccs@gmail.com>
2025-07-25 13:01:37 +0100

    Update and rename ex00 to ex00.c

---
+++ c02/ex00.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+	i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main()
+{
+	char src[] = "hello";
+	char dest[10];
+
+	printf("Sroce: %s\n", src);
+	ft_strcpy(dest, src);
+	printf("Destination: %s\n", dest);
+	return (0);
+}*/
