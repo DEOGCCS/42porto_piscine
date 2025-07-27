6ec72060464fd13432c4b5d25a4dedd96bffc0e1
David Gomes <davidoliveiraccs@gmail.com>
2025-07-27 23:40:58 +0100

    Update ex03.c

--- c02/ex03.c
+++ c02/ex03.c
@@ -9,20 +9,18 @@ int     ft_str_is_numeric(char *str)
         {
                 if (!(str[i] >= '0' && str[i] <= '9'))
                 {
-                        printf("0\n");
                         return (0);
                 }
                 i++;
         }
 
-        printf("1\n");
         return (1);
 }
 
 int     main()
 {
-        ft_str_is_numeric("1234567890");
-        ft_str_is_numeric("abc");
-        ft_str_is_numeric("");
+        printf("%d", ft_str_is_numeric("1234567890"));
+        printf("%d", ft_str_is_numeric("abc"));
+        printf("%d", ft_str_is_numeric(""));
         return(0);
 }
