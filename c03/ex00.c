3c6faa49fa0ac2509c18e180e6da845df58e38f4
David Gomes <davidoliveiraccs@gmail.com>
2025-07-28 16:36:38 +0100

    Update ex00.c

--- c03/ex00.c
+++ c03/ex00.c
@@ -5,11 +5,21 @@ int ft_strcmp(char *s1, char *s2)
 	int	i;
 	
 	i = 0;
-	while (s1[i], s2[i])
+	while (s1[i] && s2[i])
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (1);
+			return (s1[i] - s2[i]);
 		}
 		i++;
-		if else (s:
+	return (s1[i] - s2[i]);
+}
+
+int 	main(void)
+{
+	char s1[] = "hello";
+	char s2[] = "helly";
+
+	printf("%d\n", ft_strcmp(s1, s2));
+	return (0);
+}
