364720d97fd3a090d51908a5348fbbfd74fec6b4
David Gomes <davidoliveiraccs@gmail.com>
2025-07-28 09:01:04 +0100

    Update ex07.c

--- c02/ex07.c
+++ c02/ex07.c
@@ -9,7 +9,7 @@ char *ft_strupcase(char *str)
   {
       if (str[i] >= 'a' && str[i] <= 'z')
       {
-        str[i] = str[i] - ('a' - 'A'); // a=97 e A=41, ex: 97-(97-41) = 97-56 = 41
+        str[i] = str[i] - ('a' - 'A'); // a=97 e A=65, ex: 97-(97-65) = 97-32 = 65
       }
         i++;
   }
