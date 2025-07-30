3c2f6324d51201de9111b4bbf64d4876edd855b8
David Gomes <davidoliveiraccs@gmail.com>
2025-07-30 16:59:19 +0100

    Update ft_strlen.c

--- c04/ex00/ft_strlen.c
+++ c04/ex00/ft_strlen.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+*/
+int	ft_strlen(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	main(void)
+{
+	printf("Length: %d\n", ft_strlen("Hello, world!"));
+	return (0);
+}
