645e9c60b87a95c1077927dc2f0dafc4761a8305
David Gomes <davidoliveiraccs@gmail.com>
2025-08-04 12:34:20 +0100

    Update ex03 atoi.c

--- c04 /ex03 atoi.c
+++ c04 /ex03 atoi.c
@@ -1,46 +1,41 @@
-#include <stdio.h>
-  
+//#include <stdio.h>
+
 int	ft_atoi(char *str)
 {
 	int	i;
-	int count_n;
-	int sinal = 1;
-	int number = 0;
+	int	number;
+	int	signal;
 
 	i = 0;
-	count_n = 0;
-	while (str[i])
+	number = 0;
+	signal = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while ((*str == '-') || (*str == '+'))
 	{
-		while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13 ))
-			i++;
-		while (str[i] == '-')
-		{
-			count_n++;
-			if (str[i] == '+')
-				i++;
-			i++;
-		}
-		if(count_n % 2 != 0)
-			sinal = -1;
-		if((str[i] >= '0') && (str[i] <= '9'))
-			number = (number * 10) + (str[i] - '0');
+		if (*str == '-')
+			signal = signal * -1;
+		str++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = (number * 10) + (str[i] - '0');
 		i++;
 	}
-	return(sinal*number);
+	return (signal * number);
 }
-
+/*
 int	main(void)
 {
-	// Testes básicos
-	printf("Resultado 1: %d\n", ft_atoi("42"));           // Esperado: 42
-	printf("Resultado 2: %d\n", ft_atoi("   123"));       // Esperado: 123
-	printf("Resultado 3: %d\n", ft_atoi("-42"));          // Esperado: -42
-	printf("Resultado 4: %d\n", ft_atoi("--42"));         // Esperado: 42 (2 sinais negativos = positivo)
-	printf("Resultado 5: %d\n", ft_atoi("+-42"));         // Esperado: 0 ou comportamento indefinido
-	printf("Resultado 6: %d\n", ft_atoi("  -00123abc"));  // Esperado: -123
-	printf("Resultado 7: %d\n", ft_atoi("   +42"));       // Esperado: 42
-	printf("Resultado 8: %d\n", ft_atoi("  ---+--+123")); // Esperado: -123 (3 sinais negativos = ímpar = negativo)
-	printf("Resultado 9: %d\n", ft_atoi("abc"));          // Esperado: 0
-
+	printf("Resultado 1: %d\n", ft_atoi("42"));
+	printf("Resultado 2: %d\n", ft_atoi("   123"));
+	printf("Resultado 3: %d\n", ft_atoi("-42"));
+	printf("Resultado 4: %d\n", ft_atoi("--42"));
+	printf("Resultado 5: %d\n", ft_atoi("+-42"));
+	printf("Resultado 6: %d\n", ft_atoi("  -00123abc"));
+	printf("Resultado 7: %d\n", ft_atoi("   +42"));
+	printf("Resultado 8: %d\n", ft_atoi("  ---+--+123"));
+	printf("Resultado 9: %d\n", ft_atoi("abc"));
+	
 	return 0;
-}
+}*/
